4ab0d085113f5de3a5bf8e20d223e61f17f0ad0a
yuening123456 <qw1007260923@qq.com>
2017-05-19 18:03:28 +0800

    添加处理返回结果的回调接口

--- superwechat/src/cn/ucai/superwechatui/data/OnCompleteListener.java
+++ superwechat/src/cn/ucai/superwechatui/data/OnCompleteListener.java
@@ -4,6 +4,7 @@ package cn.ucai.superwechatui.data;
  * Created by Administrator on 2017/5/19 0019.
  */
 
-public interface OnCompleteListener<T> extends OkHttpUtils.OnCompleteListener<T> {
+public interface OnCompleteListener<T> extends
+        OkHttpUtils.OnCompleteListener<T> {
 
 }
