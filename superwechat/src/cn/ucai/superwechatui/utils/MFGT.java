7d9df5585cde7eec656828a3652eb89da5831755
yuening123456 <qw1007260923@qq.com>
2017-05-19 15:05:56 +0800

    添加跳转的工具类

---
+++ superwechat/src/cn/ucai/superwechatui/utils/MFGT.java
@@ -0,0 +1,28 @@
+package cn.ucai.superwechatui.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import cn.ucai.superwechatui.R;
+import cn.ucai.superwechatui.ui.LoginActivity;
+import cn.ucai.superwechatui.ui.MainActivity;
+import cn.ucai.superwechatui.ui.SplashActivity;
+
+/**
+ * Created by Administrator on 2017/5/19 0019.
+ */
+
+public class MFGT  {
+    private static void startActivity(Context context,Class clazz){
+        context.startActivity(new Intent(context,clazz));
+        ((Activity)context).overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+    }
+    public static void gotoLogin(Activity activity) {
+        startActivity(activity, LoginActivity.class);
+    }
+
+    public static void gotoMain(SplashActivity activity) {
+        startActivity(activity, MainActivity.class);
+    }
+}
