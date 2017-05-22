5da3dbb4833fbb2f1e44c4ac2d41967646a7d3ce
yuening123456 <qw1007260923@qq.com>
2017-05-22 15:22:24 +0800

    仿写环信异步加载数据的逻辑完成App用户数据的加载

--- superwechat/src/cn/ucai/superwechatui/ui/LoginActivity.java
+++ superwechat/src/cn/ucai/superwechatui/ui/LoginActivity.java
@@ -160,7 +160,7 @@ public class LoginActivity extends BaseActivity {
                     pd.dismiss();
                 }
                 // get user's info (this should be get from App's server or 3rd party service)
-                SuperWeChatHelper.getInstance().getUserProfileManager().asyncGetCurrentUserInfo();
+                SuperWeChatHelper.getInstance().getUserProfileManager().asyncGetCurrentAppUserInfo();
 
                 Intent intent = new Intent(LoginActivity.this,
                         MainActivity.class);
