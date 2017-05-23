2689b1e06b06731b541fa8463f67f6880a860d20
yuening123456 <qw1007260923@qq.com>
2017-05-23 11:46:13 +0800

    创建个人中心的Fragment和布局文件

---
+++ superwechat/src/cn/ucai/superwechatui/ui/ContactFragment.java
@@ -0,0 +1,81 @@
+package cn.ucai.superwechatui.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+import cn.ucai.easeui.domain.User;
+import cn.ucai.easeui.ui.EaseBaseFragment;
+import cn.ucai.superwechatui.R;
+import cn.ucai.superwechatui.SuperWeChatHelper;
+
+/**
+ * Created by Administrator on 2017/5/23 0023.
+ */
+
+public class ContactFragment extends EaseBaseFragment {
+    @BindView(R.id.iv_avatar)
+    ImageView ivAvatar;
+    @BindView(R.id.tv_nickname)
+    TextView tvNickname;
+    @BindView(R.id.tv_wexinhao)
+    TextView tvWexinhao;
+    Unbinder bind;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = View.inflate(getContext(), R.layout.contact_fragment, null);
+        bind = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void setUpView() {
+        User user = SuperWeChatHelper.getInstance().getUserProfileManager().getCurrentAppUserInfo();
+        if(user!=null){
+            tvNickname.setText(user.getMUserNick());
+            tvWexinhao.setText("微信号："+user.getMUserName());
+            if(!TextUtils.isEmpty(user.getAvatar())){
+                Glide.with(getContext()).load(user.getAvatar()).placeholder(R.drawable.em_default_avatar).
+                        into(ivAvatar);
+            }else{
+                Glide.with(getContext()).load(R.drawable.em_default_avatar).into(ivAvatar);
+            }
+        }
+
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        bind.unbind();
+    }
+
+    @OnClick({R.id.txt_money, R.id.txt_setting})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.txt_money:
+                break;
+            case R.id.txt_setting:
+                break;
+        }
+    }
+}
